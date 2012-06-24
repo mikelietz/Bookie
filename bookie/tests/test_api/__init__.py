30521a1ad1e5ae7cc7b510c401498fb9ee287a35
Richard Harding <rharding@mitechie.com>
2012-06-23 23:48:39 -0400

    Change over to using breadability vs decruft for readable parsing

--- bookie/tests/test_api/__init__.py
+++ bookie/tests/test_api/__init__.py
@@ -56,7 +56,7 @@ class BookieAPITest(unittest.TestCase):
         # if we want to test the readable fulltext side we want to make sure we
         # pass content into the new bookmark
         if content:
-            prms['content'] = "<h1>There's some content in here dude</h1>"
+            prms['content'] = "<p>There's some content in here dude</p>"
 
         # req_params = urllib.urlencode(prms)
         res = self.testapp.post('/api/v1/admin/bmark?',
